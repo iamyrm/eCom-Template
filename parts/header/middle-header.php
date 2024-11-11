80dddaa1cbd29cee065b6b3b786dd2ced78187d1
Yagyaraj Majhi <majhiyagya123@gmail.com>
2024-11-11 20:22:39 +0545

    cart page templating done

--- parts/header/middle-header.php
+++ parts/header/middle-header.php
@@ -109,8 +109,8 @@
           </div><!-- End .dropdown-cart-total -->
 
           <div class="dropdown-cart-action">
-            <a href="cart.html" class="btn btn-primary">View Cart</a>
-            <a href="checkout.html" class="btn btn-outline-primary-2"><span>Checkout</span><i
+            <a href="../../cart.php" class="btn btn-primary">View Cart</a>
+            <a href="checkout.php" class="btn btn-outline-primary-2"><span>Checkout</span><i
                 class="icon-long-arrow-right"></i></a>
           </div><!-- End .dropdown-cart-total -->
         </div><!-- End .dropdown-menu -->
